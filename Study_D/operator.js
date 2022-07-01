da50ea9f19167f08c9b0d3126306d3e5755b1ca4
hyunjung <hazelc3837@gmail.com>
2022-07-01 16:48:24 +0900

    add: declarative & imperative

---
+++ Study_D/operator.js
@@ -0,0 +1,15 @@
+//1. String concatenation
+
+//2.Numeric operatons (사칙연산)
+
+//3. Increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+//counter = counter +1;
+//preIncrement = counter;
+
+console.log(`preIncrement : ${preIncrement},counter: ${counter}`);
+
+const postincrement = counter++;
+//postIncrement = counter;
+//counter= counter+1;
