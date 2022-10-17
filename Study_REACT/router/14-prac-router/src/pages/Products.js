0e3a0b1b6359207e6f47c344f1ac1a6c26484419
hyunjung <hazelc3837@gmail.com>
2022-10-17 19:02:14 +0900

    react router Link & NavLink

---
+++ Study_REACT/router/14-prac-router/src/pages/Products.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <section>
+      <h1>제품 리스트</h1>
+      <ul>
+        <li>마들렌</li>
+        <li>휘낭시에</li>
+        <li>까눌레</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
---
+++ Study_REACT/router/14-prac-router/src/pages/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>웰컴 페이지!</h1>
+    </div>
+  );
+};
+
+export default Welcome;
