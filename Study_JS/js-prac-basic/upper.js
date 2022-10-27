aa3caa61dfe73519197f8f1a3fbc9c6303192d33
hyunjung <hazelc3837@gmail.com>
2022-10-27 18:12:53 +0900

    add:js-prac-basic toUpperCase,toLoweCase

---
+++ Study_JS/js-prac-basic/upper.js
@@ -0,0 +1,18 @@
+//전체 대문자 변경
+let a = "ItisSuNnYdAy";
+
+const result = a.toUpperCase();
+
+console.log(result);
+
+// function result(a) {
+//   let answer = "";
+//   for (let x of a) {
+//     if (x === x.toLowerCase()) answer += x.toUpperCase();
+//     else answer += x;
+//   }
+//   return answer;
+// }
+
+// let a = "ItisSuNnYdAy";
+// console.log(result(a));
---
+++ Study_JS/js-prac-basic/upperlower.js
@@ -0,0 +1,10 @@
+function result(a) {
+  let answer = "";
+  for (let x of a) {
+    if (x === x.toUpperCase()) answer += x.toLowerCase();
+    else answer += x.toUpperCase();
+  }
+  return answer;
+}
+
+console.log(result("hAPPY dAY"));
