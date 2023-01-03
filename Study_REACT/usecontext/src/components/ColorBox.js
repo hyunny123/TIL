3473f9c96696d37802bb4eb1dc57161c285dee69
hyunjung <hazelc3837@gmail.com>
2023-01-03 11:05:26 +0900

    add:contextAPI-practice-useContext

--- Study_REACT/usecontext/src/components/ColorBox.js
+++ Study_REACT/usecontext/src/components/ColorBox.js
@@ -1,27 +1,25 @@
-import { ColorConsumer } from "../context/color";
+import { useContext } from "react";
+import ColorContext from "../context/color";
 
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
-    <ColorConsumer>
-      {(value) => (
-        <>
-          <div
-            style={{
-              width: "100px",
-              height: "100px",
-              background: value.state.color,
-            }}
-          />
-          <div
-            style={{
-              width: "50px",
-              height: "50px",
-              background: value.state.subColor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "100px",
+          height: "100px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "50px",
+          height: "50px",
+          background: state.subColor,
+        }}
+      />
+    </>
   );
 };
 
