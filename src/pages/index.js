6ecf389e288e1c34a6a72363583ab8e8923b29b2
hyunjung <hazelc3837@gmail.com>
2023-03-19 15:28:14 +0900

    add:getServerSideProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,20 @@ import { useEffect, useState } from "react";
 import Seo from "../../components/Seo";
 import styles from "./index.module.css";
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
+  // const [movies, setMovies] = useState([]);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { results } = await (await fetch(`/api/movies`)).json();
+  //     setMovies(results);
+  //   })();
+  // }, []);
   return (
     <div className={styles.container}>
       <Seo title="Home" />
       {/* <h1>Welcome!HomePage</h1> */}
       {/* {!movies && <h4>Loading...</h4>} */}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className={styles.movie} key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -28,4 +28,12 @@ export default function Home() {
 
 export async function getServerSideProps() {
   //코드는 서버로 돌아감.
+  const { results } = await (
+    await fetch(`http://localhost:3001/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
