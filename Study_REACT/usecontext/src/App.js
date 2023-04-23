4766711cb19d00aaea043203713b7f184bac74bd
hyunjung <hazelc3837@gmail.com>
2023-04-23 20:51:36 +0900

    Add react basic state- 이름바꾸기

--- Study_REACT/usecontext/src/App.js
+++ Study_REACT/usecontext/src/App.js
@@ -25,19 +25,50 @@
 
 //컨텍스트 API
 
-import React from "react";
-import ColorBox from "./components/ColorBox";
-import { ColorProvider } from "./context/color";
-import SelectColor from "./components/SelectColor";
+// import React from "react";
+// import ColorBox from "./components/ColorBox";
+// import { ColorProvider } from "./context/color";
+// import SelectColor from "./components/SelectColor";
+
+// const App = () => {
+//   return (
+//     <ColorProvider>
+//       <div>
+//         <SelectColor />
+//         <ColorBox />
+//       </div>
+//     </ColorProvider>
+//   );
+// };
+
+// export default App;
+
+//
+
+import React, { useState } from "react";
 
 const App = () => {
+  const [user, setUser] = useState({
+    name: "jay",
+    email: "jay@gmail.com",
+    images: ["jay.png", "jay01.png"],
+  });
+  const [input, setInput] = useState("");
+
+  const changeHandle = (e) => {
+    setInput(e.target.value);
+  };
+
+  const clickHandle = () => {
+    setUser((prev) => ({ ...prev, name: input }));
+  };
   return (
-    <ColorProvider>
-      <div>
-        <SelectColor />
-        <ColorBox />
-      </div>
-    </ColorProvider>
+    <div>
+      <h3>사용자:</h3>
+      <input onChange={changeHandle} placeholder="새로운 이름을 써주세요" />
+      <button onClick={clickHandle}>수정하기</button>
+      <span>새로운 이름:{user.name}</span>
+    </div>
   );
 };
 
