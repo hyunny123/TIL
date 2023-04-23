9b5dfed2b3fe27b2e1893ddb60ef39ea7eb6561e
hyunjung <hazelc3837@gmail.com>
2023-04-23 21:16:25 +0900

    add:algorithm pro-모스부호1

---
+++ Study_JS/js-prac-basic/pro/모스부호.js
@@ -0,0 +1,45 @@
+function solution(letter) {
+  let answer = "";
+  const morse = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+  };
+
+  const word = letter.split("");
+  for (let i = 0; i < word.length; i++) {
+    answer += morse[word[i]];
+  }
+  return answer;
+}
+
+다른답
+function solution(letter) {
+  return letter
+    .split("")
+    .map((v) => morse[v])
+    .join("");
+}
