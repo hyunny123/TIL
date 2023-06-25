e761a9f8c68ff7e3f3ecbf2a466eb63be9ce1196
hyunny123 <hazelc3837@gmail.com>
2023-06-25 16:19:29 +0900

    FEAT: pager 기능 추가

--- Study_JS/js-복습/carousel/js/main.js
+++ Study_JS/js-복습/carousel/js/main.js
@@ -2,14 +2,21 @@ let slideWrapper = document.querySelector(".slide-wrapper");
 let slideContainer = slideWrapper.querySelector(".slide-container");
 let slides = slideContainer.querySelectorAll("li");
 let slideCount = slides.length;
+let currentSlideIdx = 0;
+let pager = slideWrapper.querySelector(".pager");
+let timer;
+let pagerHTML = "";
 let prevBtn = slideWrapper.querySelector("#prev");
 let nextBtn = slideWrapper.querySelector("#next");
 
 if (slideCount > 1) {
   slides.forEach((slide, index) => {
     slide.style.left = `${index * 100}%`;
+    pagerHTML += `<a href="">${index}</a>`;
   });
 }
+pager.innerHTML = pagerHTML;
+let pagerBtn = pager.querySelectorAll("a");
 
 function moveSlide(num) {
   slideContainer.style.left = `${-num * 100}%`;
@@ -34,6 +41,12 @@ function moveSlide(num) {
     sl.classList.remove("active");
   }
   slides[currentSlideIdx].classList.add("active");
+
+  //모든 페이저에서 active를 제거하고, 현재 슬라이드 번호의 pager에 active 추가
+  for (let pb of pagerBtn) {
+    pb.classList.remove("active");
+  }
+  pagerBtn[currentSlideIdx].classList.add("active");
 }
 
 moveSlide(0);
@@ -51,6 +64,20 @@ prevBtn.addEventListener("click", () => {
   }
 });
 
+//페이저로 슬라이드 이동
+/*
+페이저 버튼을 클릭하면
+링크의 기본 속성 막기
+moveSlide에 클릭한 그 요소의 인덱스 번호를 넘긴다.
+*/
+
+pagerBtn.forEach((item, idx) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    moveSlide(idx);
+  });
+});
+
 //자동 슬라이드
 function autoSlide() {
   timer = setInterval(() => {
@@ -62,11 +89,10 @@ function autoSlide() {
 autoSlide();
 //마우스가 slideWrapper에 over 하거나 out했을시 자동슬라이드 멈추기
 
-slideWrapper.addEventListener("mouseover",()=>{
-    clearInterval(timer);
-})
+slideWrapper.addEventListener("mouseover", () => {
+  clearInterval(timer);
+});
 
-slideWrapper.addEventListener("mouseout",()=>{
-    autoSlide();
-})
-
+slideWrapper.addEventListener("mouseout", () => {
+  autoSlide();
+});
