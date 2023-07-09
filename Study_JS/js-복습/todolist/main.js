8af487ceb1ad79476b6513287f79f9573c237d3b
hyunny123 <hazelc3837@gmail.com>
2023-07-10 00:32:31 +0900

    FEAT:todolist - tabs

--- Study_JS/js-복습/todolist/main.js
+++ Study_JS/js-복습/todolist/main.js
@@ -6,8 +6,17 @@ false이면 안끝난걸로 간주하고 그래도 보여준다
 let taskInput = document.getElementById("task-input");
 let addBtn = document.getElementById("add-button");
 let taskList = [];
+let tabs = document.querySelectorAll(".task-tabs div");
+let mode = "all";
+let filterList = [];
 
 addBtn.addEventListener("click", addTask);
+//시작을 1부터하는 이유는  0번째는 언더라인이기떄문
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", (e) => {
+    filter(e);
+  });
+}
 
 function addTask() {
   let task = {
@@ -22,22 +31,29 @@ function addTask() {
 
 //그리는 함수
 function render() {
+  let list = [];
+  if (mode == "all") {
+    list = taskList;
+  } else if (mode == "ing" || mode == "done") {
+    list = filterList;
+  }
+
   let resultHTML = "";
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete == true) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
       resultHTML += `<div class="task">
-          <div class="task-done">${taskList[i].taskContent}</div>
+          <div class="task-done">${list[i].taskContent}</div>
           <div>
-            <button onclick="toggleComplete('${taskList[i].id}')">check</button>
-            <button onclick="deleteTask('${taskList[i].id}')">delete</button>
+            <button onclick="toggleComplete('${list[i].id}')">check</button>
+            <button onclick="deleteTask('${list[i].id}')">delete</button>
           </div>
         </div>`;
     } else {
       resultHTML += ` <div class="task">
-        <div>${taskList[i].taskContent}</div>
+        <div>${list[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">check</button>
-          <button onclick="deleteTask('${taskList[i].id}')">delete</button>
+          <button onclick="toggleComplete('${list[i].id}')">check</button>
+          <button onclick="deleteTask('${list[i].id}')">delete</button>
         </div>
       </div>`;
     }
@@ -73,6 +89,36 @@ function deleteTask(id) {
   }
   render();
   console.log(taskList);
+}
+
+function filter(e) {
+  mode = e.target.id;
+  filterList = []; //지역변수
+
+  document.getElementById("under-line").style.width =
+    e.target.offsetWidth + "px";
+  document.getElementById("under-line").style.top =
+    e.target.offsetTop + e.target.offsetHeight + "px";
+  document.getElementById("under-line").style.left = e.target.offsetLeft + "px";
+  if (mode == "all") {
+    render();
+  } else if (mode == "ing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  } else if (mode == "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  }
+
+  console.log(filterList);
 }
 
 //랜덤 아이디 함수
